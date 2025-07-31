fabbf0c3a89b9e869430e51e9c8aad1ded15a210
Vitaliy Kobtsev <v@kobaa.ru>
2025-07-31 18:34:01 +0400

    Добавил описание задачи и комментарии к решению.

---
+++ stepik/Поколение Python курс для профессионалов/Тип данных Counter/Функция print_bar_chart().py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+
+def print_bar_chart(data, mark):
+    counter = Counter()
+
+    if type(data) == list:
+
+        for w in data:
+            counter.update([w])
+        max_len = max(map(len, counter))
+
+        for l, n in counter.most_common():
+            print(f'{l.ljust(max_len)} |{mark * n}')
+
+    else:
+        counter = Counter(data)
+        for l, n in counter.most_common():
+            print(f'{l} |{mark * n}')
